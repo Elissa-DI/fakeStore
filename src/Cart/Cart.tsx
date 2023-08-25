59ccc24ebc291e82c67729fe480bb7bab231917f
Elissa <elissadusabe@gmail.com>
2023-08-25 03:25:31 +0200

    new features

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,3 +1,23 @@
 import CartItem from '../CartItem/CartItem';
 import { Wrapper } from './Cart.styles';
 import { CartItemType } from '../App';
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+    return (
+        <Wrapper>
+            <h2>Your shopping Cart</h2>
+            { cartItems.length === 0 ? <p>No items yet in cart.</p> : null }
+            { cartItems.map(item => (
+                <CartItem />
+            )) }
+        </Wrapper>
+    )
+}
+
+export default Cart
